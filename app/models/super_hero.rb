a1098c1c5c6125aaa5e86752bc836a76d66dc6de
Azhar Shah <azharshahstl@yahoo.com>
2019-05-31 22:04:32 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -2,18 +2,18 @@ class SuperHero
   
   attr_accessor :name, :power, :biography 
   
-  @@super_heroes = []
+  @@superheroes = []
   
   def initialize(args={})
     @name = args[:name]
     @power = args[:power] 
     @biography = args[:biography] 
     
-    @@super_heroes << self 
+    @@superheroes << self 
   end
   
   def self.all 
-    @@super_heroes
+    @@superheroes
   end
     
   
